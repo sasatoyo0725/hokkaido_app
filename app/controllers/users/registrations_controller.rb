bda7c77d22c3a690e0575e0a3ad15c9af9e27e3e
笹谷仁寛 <toyohiro.sasaya@gmail.com>
2020-03-11 16:56:22 +0900

    user profile image

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -10,10 +10,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # end
 
   # POST /resource
-  #def create
-     #super
-  #end
-
+  
   # GET /resource/edit
   # def edit
   #   super
