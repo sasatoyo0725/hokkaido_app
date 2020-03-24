d87618b194da9dbe9cf43f3398b97d2c9adfacf2
笹谷仁寛 <toyohiro.sasaya@gmail.com>
2020-03-24 14:22:23 +0900

    add user_spec

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,9 +1,5 @@
 FactoryBot.define do
   factory :user do
-    name { 'Example User' }
-    sequence(:email) { |n| "user_#{n}@example.com" }
-    password { 'password' }
-    password_confirmation { 'password' }
+    
   end
-
 end
