0e918d38aa39d54a19d73b2fa186cd490879e7f8
笹谷仁寛 <toyohiro.sasaya@gmail.com>
2020-03-29 16:36:18 +0900

    item model spec fix1

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,7 +1,6 @@
 FactoryBot.define do
   factory :item do
-    sequence(:description) { |n| "this place is one of my favarite place#{n}"}
-    images { [ Rack::Test::UploadedFile.new(Rails.root.join('spec/factories/test.jpg'), 'spec/factories/test.jpg') ] }  #json型で画像を保存するときは[]でデータを囲む・配列にしていないと入力エラーが発生する
-    association :user
+    user_id {1}
+    description {"RSpecでテストをかく"}
   end
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,13 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
+
   before do
-    @item = build(:item)
+    FactoryBot.create(:item)
   end
-  
+
   describe "バリデーション " do
     it 'user_idとdescriptionがあればOK' do
-      expect(@item.valid?).to eq(true)
+      
     end
 
     it 'user_idが空ならNG' do
@@ -17,6 +18,33 @@ RSpec.describe Item, type: :model do
     it 'descriptionが空ならNG' do
 
     end
+
+  end
+
+  describe "関連付け" do
+    describe "has_many" do
+    it "coments" do
+
+    end
+
+    it "likes" do
+
+    end
+
+    it "like_users" do
+    end
+
+    it "images" do
+
+    end
+  end
+
+  describe "belongs_to" do
+    it "user" do
+
+    end
+
+  end
 
   end
 
