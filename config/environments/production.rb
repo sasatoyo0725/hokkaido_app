8ce6c74377181b6df2c5b26dff71aa72f401f480
笹谷仁寛 <toyohiro.sasaya@gmail.com>
2020-03-30 18:58:19 +0900

    database.yml fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -28,6 +28,7 @@ Rails.application.configure do
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = true
+  config.assets.initialize_on_precompile=false
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = 'http://assets.example.com'
@@ -62,6 +63,7 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "hokkaido_app_production"
 
   config.action_mailer.perform_caching = false
+
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
