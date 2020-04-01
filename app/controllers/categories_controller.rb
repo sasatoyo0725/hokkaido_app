8a72fd8f2591db70ab42ad6936d2971500212636
笹谷仁寛 <toyohiro.sasaya@gmail.com>
2020-04-02 07:31:35 +0900

    category controller fix

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,7 +1,7 @@
 class CategoriesController < ApplicationController
 PER = 3
   def show
-    @category = Category.find(params[:id])
+    @category = Category.find_by(id: params[:id])
     @items = @category.items.order(created_at: :desc).page(params[:page]).per(PER).all
   end
 
